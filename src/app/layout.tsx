0ac3f9d9494d8f432b5e504d98249c77244bcf50
Adriana Alves <adrianaalves098@gmail.com>
2026-03-18 15:48:31 +0000

    fix: css bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${spaceGrotesk.variable} ${ibmPlexMono.variable}`}>
+      <body
+        suppressHydrationWarning
+        className={`${spaceGrotesk.variable} ${ibmPlexMono.variable}`}
+      >
         {children}
       </body>
     </html>
